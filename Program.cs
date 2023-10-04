c5f11a5e1ee9d1e36f9d66b11ff5de18aeb128d9
alhassenGit <alhassenbriniGit@gmail.com>
2023-10-04 15:05:46 +0200

    Auto commit on 2023-10-04 15:05:46

--- Program.cs
+++ Program.cs
@@ -3,4 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("New commit1");
 Console.WriteLine("New commit2");
 Console.WriteLine("New commit3");
+Console.WriteLine("New commit4 before restart again");
 Console.ReadLine();
