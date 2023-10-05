c6632abdb670bfb5131a33dc76546bbafc0b52ed
alhassenGit <alhassenbriniGit@gmail.com>
2023-10-05 14:05:25 +0100

    Auto commit on 2023-10-05 14:05:25

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("New commit6 already done automatically");
+
+Console.WriteLine("New commit7 already done automatically");
 Console.ReadLine();
